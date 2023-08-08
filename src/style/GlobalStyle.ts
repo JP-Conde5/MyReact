b9132c32b5156f1877ab799bf845df00f9d7aab9
jjj <jcgalves25@hotmail.com>
2023-08-08 13:02:17 -0300

    Oi

---
+++ src/style/GlobalStyle.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components"
+
+export const Padrao = createGlobalStyle`
+    *{
+        margin: 0rem;
+        padding: 0rem;
+        outline: 0rem;
+        border: 0rem;
+        box-sizing: border-box;
+    }
+` 
+
+export const cores = {
+    
+}
